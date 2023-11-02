5de231a28bb7f7d764b8cfa8369bd10a1e9805ab
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-11-02 12:15:47 -0500

    cambios

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./Estilos/Loading.module.css";
 export default function Loading() {
   return (
-    <div>
+    <div className={styles}>
       <svg viewBox="25 25 50 50">
         <circle r="20" cy="50" cx="50"></circle>
       </svg>
