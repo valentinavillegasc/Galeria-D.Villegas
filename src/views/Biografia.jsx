e12f8257ccc6d15be1e73d270a1106b9924d7218
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-12-11 16:03:48 -0500

    cambios biografia

--- src/views/Biografia.jsx
+++ src/views/Biografia.jsx
@@ -20,56 +20,28 @@ export default function Biografia() {
 
           <div className={style.textoParteDerecha}>
             <p>
-              Reconocido por su excepcional maestría en Diseño Gráfico,
-              Escultura y Artes Plásticas, Diego Armando Villegas Tafur se erige
-              como una figura sobresaliente en el vasto universo artístico.
-              Desde los albores de su apasionado viaje en el arte, trazado por
-              su formación en Diseño Gráfico en la Universidad Jorge Tadeo
-              Lozano, ha demostrado un compromiso inquebrantable con su vocación
-              creativa.
+              Diego Villegas, destacado Maestro en Artes Plásticas y artista
+              global contemporáneo, se destaca por su expresión artística
+              centrada en la abstracción de figuras de aves, como el colibrí,
+              para transmitir un mensaje profundo. Hijo de los reconocidos
+              maestros Alicia Tafur y Armando Villegas, su obra se inspira en el
+              dinamismo del vuelo y la riqueza cromática de estas criaturas
+              aladas.
             </p>
             <p>
-              La exploración constante y la búsqueda incesante de
-              enriquecimiento artístico son rasgos distintivos de la vida de
-              Diego. Más allá de su educación académica, se aventuró en el
-              Taller de Arte “Alicia Tafur”, donde experimentó con escultura,
-              fundición, cerámica y arte aplicado, sumergiéndose en diversas
-              técnicas y medios. Posteriormente, en el Taller de Arte “Armando
-              Villegas”, amplió sus horizontes creativos explorando pintura,
-              artesanía y joyería artesanal.
+              Iniciando su apasionado viaje en el arte con estudios en Diseño
+              Gráfico en la Universidad Jorge Tadeo Lozano, Diego demuestra un
+              compromiso inquebrantable con su vocación creativa. Su exploración
+              constante incluye experiencias en diversos talleres de arte, desde
+              escultura y fundición hasta pintura y joyería artesanal.
             </p>
             <p>
-              Su trayectoria profesional reluce con logros significativos. Como
-              Diseñador Gráfico, ha dejado una huella imborrable al contribuir
-              al diseño de catálogos para prestigiosas galerías y al moldear la
-              imagen corporativa del "Museo de Arte Contemporáneo Bolivariano"
-              en Santa Marta. Su impacto artístico también se manifiesta a
-              través de murales y monumentos que ha creado, fusionando
-              materiales con su imaginación, dando vida a obras memorables como
-              la escultura "LA NEGRA DEL CHONTADURO" y “LA CHAPOLERA”, fundidas
-              en bronce.
-            </p>
-            <p>
-              Pero su legado va más allá de su propia creación artística; Diego
-              también ha compartido su conocimiento y habilidades como educador
-              y mentor. Ha dejado una huella profunda como Profesor de Escultura
-              en el Colegio Gimnasio Campestre y como Profesor de Artesanía en
-              Metales y Pintura en el Colegio Helvetia, además de su papel como
-              maestro en artes plásticas en el Centro Cultural y Educativo
-              Español Reyes Católicos, y actualmente en el Colegio Gimnasio de
-              los Cerros.
-            </p>
-            <p>
-              Además, su papel como director de Casa Tafur ilustra su dedicación
-              para inspirar y nutrir las futuras generaciones de artistas. A lo
-              largo de su distinguida carrera, ha participado en exposiciones
-              colectivas y concursos, cosechando reconocimientos en diversas
-              galerías y espacios culturales. Su legado es un testimonio
-              elocuente de su dedicación incansable al arte en todas sus formas
-              y de su profundo impacto en la comunidad artística y cultural.
-              Diego Armando Villegas Tafur es un nombre que resonará eternamente
-              en la historia del arte, dejando una marca indeleble en el mundo
-              creativo.
+              Además de su contribución al diseño gráfico y la creación de obras
+              notables, Diego Villegas también se destaca como educador y
+              mentor, siendo director de Casa Tafur y participando activamente
+              en exposiciones y concursos. Su legado es un testimonio elocuente
+              de su dedicación al arte y su impacto duradero en la comunidad
+              artística y cultural.
             </p>
           </div>
         </div>
