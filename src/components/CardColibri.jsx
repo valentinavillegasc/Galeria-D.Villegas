35b0ef48d695d7fc04426adbbc67d87b3564ff53
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-02-06 16:25:15 -0500

    cambio estilos colibri card

--- src/components/CardColibri.jsx
+++ src/components/CardColibri.jsx
@@ -5,8 +5,8 @@ export default function CardColibri(props) {
   return (
     <Link className={style.link} to={`/detailColibri/${props.id}`}>
       <div className={style.card}>
-        <img src={props.image} alt="colibri" className={style.image} />
         <h2 className={style.name}>{props.name}</h2>
+        <img src={props.image} alt="colibri" className={style.image} />
       </div>
     </Link>
   );
